f6824805d307e74ba78f3ee36605690eaa84cf67
Moglan Diana <mogdiana@gmail.com>
2020-08-13 15:53:27 +0300

    lesson5

---
+++ lesson5/I_love_Java.java
@@ -0,0 +1,6 @@
+public class I_love_Java {
+
+	public static void main(String[] arg){
+		System.out.println("I love Java programming!!!");
+	}
+}
---
+++ lesson5/My_name.java
@@ -0,0 +1,6 @@
+public class My_name {
+
+	public static void main(String[] arg){
+		System.out.println("My name is Diana Moglan!!!");
+	}
+}
