89bcdf1a04944e72e882caa49dadc440ec56a5c2
Moglan Diana <mogdiana@gmail.com>
2020-08-17 20:39:59 +0300

    third commit lesson6

--- lesson6/src/Test.java
+++ lesson6/src/Test.java
@@ -1,7 +1,19 @@
 public class Test {
     public static void main(String[] args) {
         primaclasa prim = new primaclasa();
-        byte Ab=1;
-        System.out.println(Ab);
+        byte a = 3;
+        int b = -10;
+        //Incrementarea
+        a++;
+        System.out.println(a+b);
+        long x = 13129028904820l;
+        double y = x;
+        float f = 123.6783535532525f;
+        double d = 12.345904454;
+        System.out.printf("%d\n", x);
+        System.out.printf("%f\n",y);
+        System.out.println(d);
+        String l = 20 + 20 + " Hello " + 2021;
+        System.out.println(l);
     }
 }
