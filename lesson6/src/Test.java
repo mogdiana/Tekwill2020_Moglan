9f6a63dda0427e35075aff0404f54f80b7d9310d
Moglan Diana <mogdiana@gmail.com>
2020-08-24 16:12:10 +0300

    Homework

--- lesson6/src/Test.java
+++ lesson6/src/Test.java
@@ -1,7 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        primaclasa prim = new primaclasa();
-        byte a = 3;
+/*        byte a = 3;
         int b = -10;
         //Incrementarea
         a++;
@@ -15,5 +14,43 @@ public class Test {
         System.out.println(d);
         String l = 20 + 20 + " Hello " + 2021;
         System.out.println(l);
+        int s = 10;
+        switch (s){
+            case 2:
+                System.out.println("2");
+            case 10:
+                System.out.println("10");
+                break;
+            case 1:
+                System.out.println("1");
+            default: {
+                System.out.println("Nici o varianta");
+            }
+        }
+        if (false)
+            System.out.println("This ");
+        for (int i = 1; i <= 5; i++){
+            int j = 20;
+            System.out.println(j);
+        }
+        System.out.println(j);
+    */
+    Angajat angajat1 = new Angajat();
+    angajat1.printeazaVirstaSiNume();
+    long virsta = angajat1.getVirsta();
+    System.out.println(virsta);
+    long sum = virsta + 10;
+    angajat1.setNume("Andrei");
+    angajat1.setVirsta(30L);
+    System.out.println(angajat1.getVirsta());
+    angajat1.printeazaNume();
+
+    Angajat angajat2 = new Angajat();
+    System.out.println(angajat2.getVirsta());
+    angajat2.setNume("Vasile");
+    angajat2.printeazaNume();
+
     }
+
 }
+
