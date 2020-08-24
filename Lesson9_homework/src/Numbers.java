daed4b79a358eedc675a2aa10c2964aa28a3d718
Moglan Diana <mogdiana@gmail.com>
2020-08-24 15:45:50 +0300

    Homework_1

--- Lesson9_homework/src/Numbers.java
+++ Lesson9_homework/src/Numbers.java
@@ -1,11 +1,20 @@
 public class Numbers {
     public static void main (String[] args) {
-        Operations op = new Operations();
-        op.setNumb(10);
-        System.out.println(op.getNumb());
-        op.Str("Diana","Moglan");
-        System.out.println(op.getStr());
-        op.setChar('8');
-        System.out.println(op.getChar_Numb());
+        Operations op1 = new Operations();
+        op1.setNumb(10);
+        System.out.println(op1.getNumb());
+        op1.Str("Diana","Moglan");
+        System.out.println(op1.getStr());
+        op1.setChar('8');
+        System.out.println(op1.getChar_Numb());
+
+        Operations op2 = new Operations();
+        op2.setNumb(10);
+        op2.Sum_numb();
+        op2.Numb_even();
+        op2.Print_Hello();
+        op2.Numb_odd();
+        op2.setNumb(256987);
+        op2.Sum_odd();
     }
 }
--- Lesson9_homework/src/Operations.java
+++ Lesson9_homework/src/Operations.java
@@ -5,6 +5,7 @@ public class Operations {
     String s1, s2;
     char c;
 
+    /*Punctul 1*/
     public void setNumb(int n) {
         num = n;
     }
@@ -18,7 +19,7 @@ public class Operations {
 
     }
 
-    String getStr () {
+    public String getStr () {
         return s1;
     }
 
@@ -28,5 +29,55 @@ public class Operations {
 
     public int getChar_Numb() {
         return (int)c;
+    }
+
+    /*Punctul 2*/
+    /* 2.1. O metoda care calculeaza si afiseaza suma tuturor numerelor de la 0< n (n sa fie dinamic: ca argument).  */
+    void Sum_numb () {
+        System.out.print("Suma numerelor de la "+0+" pina la "+num+" = ");
+        int sum = 0;
+        for (int i = 0; i < num; i++)
+            sum += i;
+        System.out.println(sum);
+    }
+
+    /* O metoda care afiseaza toate cifrele pare pina la un nr n (n sa fie dinamic: ca argument). */
+    void Numb_even (){
+        System.out.println("Numere pare de la "+0+" pina la "+num);
+        for (int i = 0; i <= num; i++)
+            if ((i % 2) == 0) {
+                System.out.print(i+" ");
+            }
+        System.out.println();
+    }
+    /*o metoda care afiseaza la ecran "Hello" de 10 ori */
+    void Print_Hello(){
+        int i = 0;
+        while (i<num){
+            System.out.print("Hello ");
+            i++;
+        }
+        System.out.println();
+    }
+    /*O metoda care afiseaza toate cifrele impare pina la un nr n (n sa fie dinamic: ca argument). */
+    void Numb_odd (){
+        System.out.println("Numere impare de la "+0+" pina la "+num);
+        for (int i = 0; i <= num; i++)
+            if ((i % 2) == 1) {
+                System.out.print(i+" ");
+            }
+        System.out.println();
+    }
+    /*O metoda care afiseaza suma cifrelor impare ale nr n (n sa fie dinamic: ca argument). */
+    void Sum_odd (){
+        int x, k = num, sum = 0;
+        while (k != 0){
+            x = k % 10;
+            if (x % 2 != 0){
+                sum += x;
+            }
+            k = k / 10;
+        }
+        System.out.println("Suma numerelor impare nr "+num+" = "+sum);
     }
 }
